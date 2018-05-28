1ddefc232154dc2dfc3a6f884b0487084da85078
Vitor Freitas <vitorfs@gmail.com>
2018-05-28 22:28:41 +0300

    Update issue with settings ALLOWED_HOSTS default value

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='s3cr3t_k3y')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1,localhost')
 
 
 # Application definition
